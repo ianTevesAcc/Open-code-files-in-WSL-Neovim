c7726eefff39165629a1bd8d77b131564996cb6d
Ian Teves <iantevesbusinessacc@outlook.com>
2024-07-30 13:51:40 +1200

    Added batch script and readme instructions
    Added cmments on the batch script for easier understanding for the user as well as updating the read me to point out these comments

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Open-code-files-in-WSL-Neovim
-This is a batch file script that allows WSL Windows users to open their code files from inside file explorer straight to their WSL file path.
+This is a batch file script that allows WSL Windows users to open their code files from inside file explorer straight to their WSL file path. The `.bat` script is currently set to work with Ubuntu If your using a different distro, you can check out step 2 of the installation for more information.
+
+# Installation
+1. First make sure wsl is installed in your Windows machine. You can check this by running `wsl -v` in your command prompt console. If not you can install it with the following command `wsl --install` and install your chosen distro. Eg. `wsl install -d Ubuntu` to install the Ubuntu linux distro into your windows machine. You can read more here[https://learn.microsoft.com/en-us/windows/wsl/install](https://learn.microsoft.com/en-us/windows/wsl/install).
+2. Edit `open_with_nvim.bat` file in order to run in your chosen linux distro. The `.bat` script is currently set to work with the Ubuntu distro, so it needs to be configurated to suit your distro file path. Edit `:ProcessYourDistroFilePath` and `start "" Ubuntu.exe run %command%` to suit your chosen distro. (Instruction on this are commented inside the `open_with_nvim.bat` script file).
+3. Add `open_with_nvim.bat` file path into your Windows Environment Path. You can find a fleshed out tutorial here if you are unsure how to do so [https://www.architectryan.com/2018/03/17/add-to-the-path-on-windows-10/](https://www.architectryan.com/2018/03/17/add-to-the-path-on-windows-10/).
+4. Open your file explorer and right click on your chosen code file.
+    - click `Open with` and select `Choose another app`
+    - scroll down and select `choose an app on your pc`
+    - head to where you saved `open_with_nvim.bat` file path and select the `.bat` script file.
+5. Your code file should now open in your wsl distro in neovim.
---
+++ open_with_nvim.bat
@@ -0,0 +1,105 @@
+
+@echo off
+
+rem Get file address
+set "file_address=%~1"
+
+rem Replace backslashes (\) with forward slashes (/) in file path
+set "file_path=%file_address:\=/%"
+
+rem Extract directory path from file path
+for %%I in ("%file_path%") do set "dir_path=%%~dpI"
+
+rem Replace backslashes (\) with forward slashes (/) in directory path
+set "dir_path=%dir_path:\=/%"
+
+rem Remove trailing backslash from directory path
+if "%dir_path:~-1%"=="\" set "dir_path=%dir_path:~0,-1%"
+
+rem Check the first character of the file path
+set "first_char=%file_path:~0,1%"
+
+rem Filter for which Drive type the file path is
+if "%first_char%"=="C" goto ProcessWindowsFilePath
+if "%first_char%"=="D" goto ProcessWindowsFilePath
+if "%first_char%"=="E" goto ProcessWindowsFilePath
+if "%first_char%"=="F" goto ProcessWindowsFilePath
+if "%first_char%"=="G" goto ProcessWindowsFilePath
+else if "%first_char%"=="\" (
+	if "%file_path:~16,6%"=="Ubuntu" goto ProcessUbuntuFilePath
+	if "%file_path:~16,14%"=="docker-desktop" goto ProcessDockerDesktopFilePath
+	if "%file_path:~16,19%"=="docker-desktop-data" goto ProcessDockerDesktopDataFilePath
+  else goto ProcessYourDistroFilePath
+)
+
+goto End
+
+rem Clean up file path
+:ProcessWindowsFilePath
+rem Remove first 2 characters (assuming it's the drive letter and backslash)
+set "real_file_path=%file_path:~2%"
+set "real_dir_path=%dir_path:~2%"
+rem Prepend /mnt/c/ to the paths to properly reference the Windows drive in WSL
+set "ready_file_path=/mnt/c%real_file_path%"
+set "ready_dir_path=/mnt/c%real_dir_path%"
+goto End
+
+:ProcessUbuntuFilePath
+rem Remove the first 22 characters to get the Ubuntu path
+set "real_file_path=%file_path:~23%"
+set "real_dir_path=%dir_path:~23%"
+rem Prepend / to the paths to properly reference the Ubuntu drive in Linux
+set "ready_file_path=/%real_file_path%"
+set "ready_dir_path=/%real_dir_path%"
+goto End
+
+:ProcessDockerDesktopFilePath
+rem Remove the first 30 characters to get the docker-desktop path
+set "real_file_path=/%file_path:~31%"
+set "real_dir_path=/%dir_path:~31%"
+rem Prepend / to the paths to properly reference the docker-desktop drive in Linux
+set "ready_file_path=/%real_file_path%"
+set "ready_dir_path=/%real_dir_path%"
+goto End
+
+:ProcessDockerDesktopDataFilePath
+rem Remove the first 35 characters to get the docker-desktop-data path
+set "real_file_path=/%file_path:~36%"
+set "real_dir_path=/%dir_path:~36%"
+rem Prepend / to the paths to properly reference the docker-desktop-data drive in Linux
+set "ready_file_path=/%real_file_path%"
+set "ready_dir_path=/%real_dir_path%"
+goto End
+
+rem Your Distro Config Here
+:ProcessYourDistroFilePath
+rem Here we want to remove the first characters of the file path as they are set as the Windows file path. 
+rem To do this were gonna be removing the first 16 characters which is `\\wsl.localhost`. 
+rem Then we need to remove the amount of character of your distro file path name. 
+rem Eg. If you installed debian we need to remove an additional 6 characters.
+rem So we need to remove a total of 16+6+1 = 22 characters. (The plus 1 is the extra /)
+rem So in the following you need to edit the number in`%file_path:~00% underneath, in order to suit your distro file path.` 
+set "real_file_path=/%file_path:~00%"
+set "real_dir_path=/%dir_path:~00%"
+rem Then we want to Prepend / to the paths to reference them properly. (no edits needed here)
+set "ready_file_path=/%real_file_path%"
+set "ready_dir_path=/%real_dir_path%"
+
+:End
+
+rem Set command to change directory and open file in nvim
+rem "command=cd \"%ready_dir_path%\" && nvim \"%ready_file_path%\""
+rem set "command=cd \"%ready_dir_path%\""
+set "command=nvim \"%ready_file_path%\""
+
+rem Copy file & directory path into clipboard
+rem echo "%first_char%" | clip
+rem echo "%ready_file_path%" | clip
+rem echo "%ready_dir_path%" | clip
+rem echo "%command%" | clip
+
+echo "%ready_file_path%" | clip
+
+rem Launch ubuntu.exe with cmd.exe commands
+rem Edit `ubuntu.exe` to your distro command. (Eg. debian.exe, etc.)
+start "" ubuntu.exe run %command%
